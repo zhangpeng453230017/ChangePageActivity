6c4077802cc027d4cdeb47c17e9f33e75fa86d82
zhangpeng <453230017@qq.com>
2016-11-03 10:12:10 +0800

    dd

---
+++ app/src/main/java/yunzai/com/changepageactivity/MyView.java
@@ -0,0 +1,22 @@
+package yunzai.com.changepageactivity;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Content:自定义View实现翻页效果
+ * Created by zhangpeng on 2016/7/25.
+ */
+public class MyView extends View {
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
